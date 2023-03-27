d5a3aacbdf76b9a296c5d96ea7b49e357ec2530e
shinnewjean <dbwks97@gmail.com>
2023-03-27 09:47:56 +0900

    230327_PHP9

--- PHP/Training/04_2_tng1_loop.php
+++ PHP/Training/04_2_tng1_loop.php
@@ -1,7 +1,8 @@
 <?php
 //1. 반복문을 이용해서 아래와 같이 출력해 주세요.
 //갯수는 내가 입력한 갯수만큼
-$star = "*";
+
+// 1. 원본
 $endNum = 10 ;
 for ($num1=1; $num1 <= $endNum ; $num1++) { 
     for ($num2=1; $num2 <= $num1 ; $num2++) 
@@ -11,12 +12,15 @@ for ($num1=1; $num1 <= $endNum ; $num1++) {
     echo "\n";
 }
 
+// 2.트리
 echo str_repeat(" ", $endNum-1)."★\n";
 for ($i=1; $i <= $endNum ; $i++) { 
     echo str_repeat(" ", $endNum-$i);
     echo str_repeat("O", $i);
     echo str_repeat("O", $i)."\n";
 }
+
+// 3. 위아래 역방향
 for ($num1=$endNum; $num1 > 0 ; $num1--) { 
     for ($num2=1; $num2 <= $num1 ; $num2++) 
         { 
